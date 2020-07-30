916d9955a639d3460f70c75c7cb280f53774af7b
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-07-30 18:52:01 -0300

    commit

--- alura.py
+++ alura.py
@@ -5,3 +5,12 @@ class Funcionario:
     
     def mostrar_tarefas(self):
         print('Fez muita coisa...')
+
+class Caelum(Funcionario):
+
+    def mostrar_tarefas(self):
+        print('Fez muita coisa, Caelumer')
+    
+    def busca_cursos_do_mes(self, mes=None):
+        print(f'Mostrando cursos - {mes}' if mes else 'Mostrando cursos desse mês')
+
