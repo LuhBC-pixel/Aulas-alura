da5b3adf8ccd382d2bff0b8ad78ea7c251a6c5e9
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-07-30 19:01:59 -0300

    commit

--- alura.py
+++ alura.py
@@ -27,3 +27,11 @@ class Junior(Alura):
 
 class Pleno(Alura, Caelum):
     pass
+
+jose = Junior()
+jose.busca_perguntas_sem_resposta()
+
+luan = Pleno()
+luan.busca_perguntas_sem_resposta()
+luan.busca_cursos_do_mes()
+luan.mostrar_tarefas() 
