90f9b1665693f5134e67ae9c8507e1bd47bb6ae5
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-07-30 22:21:20 -0300

    Update
    Atualizei algumas coisas referente as aulas

--- alura.py
+++ alura.py
@@ -1,4 +1,7 @@
 class Funcionario:
+
+    def __init__(self, nome):
+        self.nome = nome
     
     def registra_horas(self, horas):
         print('Horas registradas.')
@@ -22,16 +25,22 @@ class Alura(Funcionario):
     def busca_perguntas_sem_resposta(self):
         print('Mostrando perguntas não respondidas do fórum')
 
+class Hipster:
+    def __str__(self):
+        return f'Hipster, {self.nome}'
+
 class Junior(Alura):
     pass
 
-class Pleno(Alura, Caelum):
+class Pleno(Alura, Caelum, Hipster):
     pass
 
-jose = Junior()
+jose = Junior('José')
 jose.busca_perguntas_sem_resposta()
 
-luan = Pleno()
+luan = Pleno('Luan')
 luan.busca_perguntas_sem_resposta()
 luan.busca_cursos_do_mes()
 luan.mostrar_tarefas() 
+
+print(luan)
