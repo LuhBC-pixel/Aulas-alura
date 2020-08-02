0ec38be4014740134f41839c5fd4ce813123025e
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-08-02 14:42:12 -0300

    Modificações

--- ExtratorArgumentosUrl.py
+++ ExtratorArgumentosUrl.py
@@ -1,4 +1,13 @@
 class ExtratorArgumentosUrl:
 
     def __init__(self, url):
-        self.url = url
+        if self.urlEhValida(url):
+            self.url = url
+        else:
+            raise LookupError("Url inválida!")
+
+    def urlEhValida(self, url):
+        if url:
+            return True
+        else:
+            return False
