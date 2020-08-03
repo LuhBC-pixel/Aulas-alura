df3c1cfa1752f2e599b23e1ab5284b73fecb8756
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-08-03 15:32:10 -0300

    aula 04

--- ExtratorArgumentosUrl.py
+++ ExtratorArgumentosUrl.py
@@ -13,11 +13,17 @@ class ExtratorArgumentosUrl:
             return False
 
     def extraiArgumentos(self):
-        indiceInicialMoedaDestino = self.url.find("=", 15) + 1
+        buscaMoedaOrigem = "moedaorigem"
+        buscaMoedaDestino = "moedadestino"
 
-        indiceInicialMoedaOrigem = self.url.find("=") + 1
+        indiceInicialMoedaDestino = self.encontraIndiceInicial(buscaMoedaDestino)
+
+        indiceInicialMoedaOrigem = self.encontraIndiceInicial(buscaMoedaOrigem)
         indiceFinalMoedaOrigem = self.url.find("&")
 
         moedaOrigem = self.url[indiceInicialMoedaOrigem:indiceFinalMoedaOrigem]
         moedaDestino = self.url[indiceInicialMoedaDestino:]
         return moedaOrigem, moedaDestino
+
+    def encontraIndiceInicial(self, moedaBuscada):
+        return self.url.find(moedaBuscada) + len(moedaBuscada) + 1
