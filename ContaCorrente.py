f30a87fbc0a8a2682966bd1eca8155b5dadc0aab
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-08-09 14:45:05 -0300

    novas aulas

--- ContaCorrente.py
+++ ContaCorrente.py
@@ -1,1 +1,11 @@
-class contaCorre
+class contaCorrente:
+
+    def __init__(self, codigo):
+        self.codigo = codigo
+        self.saldo = 0
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def __str__(self):
+        return f'[>>Codigo {self.codigo} Saldo {self.saldo} <<]'
