4a5fbb6f4d99e01d7e6f8956c25c19ab6ebdd313
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-08-10 19:27:36 -0300

    Update ContaCorrente.py

--- ContaCorrente.py
+++ ContaCorrente.py
@@ -1,11 +1,23 @@
-class contaCorrente:
+class Conta:
 
     def __init__(self, codigo):
-        self.codigo = codigo
-        self.saldo = 0
+        self._codigo = codigo
+        self._saldo = 0
 
     def deposita(self, valor):
-        self.saldo += valor
+        self._saldo += valor
 
     def __str__(self):
-        return f'[>>Codigo {self.codigo} Saldo {self.saldo} <<]'
+        return f'[>>Codigo {self._codigo} Saldo {self._saldo} <<]'
+
+class ContaCorrente(Conta):
+
+    def passa_o_mes(self):
+        self._saldo -= 2
+
+class ContaPoupanca(Conta):
+
+    def passa_o_mes(self):
+        self._saldo += 1.01
+        self._saldo -= 3
+        
