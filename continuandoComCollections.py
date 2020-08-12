495ac8fd75269ead9088d9637b70c328ef9f9d20
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-08-11 22:43:42 -0300

    Update continuandoComCollections.py

--- continuandoComCollections.py
+++ continuandoComCollections.py
@@ -1,3 +1,4 @@
+# AULA 01
 usuarios_data_science = [15, 23, 43, 56]
 usuarios_machine_learning = [13, 23, 56, 42]
 
@@ -41,6 +42,8 @@ fez_ds_mas_nao_fez_ml = usuarios_data_science - usuarios_machine_learning
 
 usuarios_data_science ^ usuarios_machine_learning
 
+# AULA 02
+
 usuarios = {1,5,76,34,52,13,17}
 len(usuarios)
 
@@ -63,3 +66,75 @@ meu_texto = "Bem vindo meu nome é Guilherme eu gosto muito de nomes e tenho o m
 meu_texto.split()
 
 set(meu_texto.split())
+
+# AULA 03
+
+"""# Dicionário (Mapa etc)"""
+
+aparicoes = {
+  "Guilherme" : 1,
+  "cachorro" : 2,
+  "nome" : 2,
+  "vindo" : 1
+}
+
+type(aparicoes)
+
+aparicoes["Guilherme"]
+
+aparicoes["cachorro"]
+
+aparicoes["xpto"]
+
+aparicoes.get("xpto", 0)
+
+aparicoes.get("cachorro", 0)
+
+aparicoes = dict(Guilherme = 2, cachorro = 1)
+aparicoes
+
+aparicoes = {
+  "Guilherme" : 1,
+  "cachorro" : 2,
+  "nome" : 2,
+  "vindo" : 1
+}
+
+aparicoes["Carlos"] = 1
+
+aparicoes
+
+aparicoes["Carlos"] = 2
+
+aparicoes
+
+del aparicoes["Carlos"]
+
+aparicoes
+
+"cachorro" in aparicoes
+
+"Carlos" in aparicoes
+
+for elemento in aparicoes:
+  print(elemento)
+
+for elemento in aparicoes.keys():
+  print(elemento)
+
+for elemento in aparicoes.values():
+  print(elemento)
+
+1 in aparicoes.values()
+
+for elemento in aparicoes.keys():
+  valor = aparicoes[elemento]
+  print(elemento, valor)
+
+for elemento in aparicoes.items():
+  print(elemento)
+
+for chave, valor in aparicoes.items():
+  print(chave, "=", valor)
+
+["palavra {}".format(chave) for chave in aparicoes.keys()]
