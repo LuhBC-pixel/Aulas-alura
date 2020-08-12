086464ef2cf7c7a3bdbcf4e1c3232f42bb2f9baa
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-08-11 22:12:47 -0300

    Create continuandoComCollections.py

---
+++ continuandoComCollections.py
@@ -0,0 +1,31 @@
+usuarios_data_science = [15, 23, 43, 56]
+usuarios_machine_learning = [13, 23, 56, 42]
+
+assistiram = usuarios_data_science.copy()
+assistiram.extend(usuarios_machine_learning)
+assistiram
+
+len(assistiram)
+
+set(assistiram)
+
+set([1,2,3,1])
+
+{4, 1,2,3,1}
+
+type({1,2})
+
+usuarios_data_science = {15, 23, 43, 56}
+usuarios_machine_learning = {13, 23, 56, 42}
+
+usuarios_machine_learning
+
+# usuarios_machine_learning[3] - dá erro porque é set(conjunto)
+
+for usuario in set(assistiram):
+  print(usuario)
+
+usuarios_data_science = {15, 23, 43, 56}
+usuarios_machine_learning = {13, 23, 56, 42}
+
+usuarios_data_science | usuarios_machine_learning
