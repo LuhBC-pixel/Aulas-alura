7b12fc06422d5c747f5675f1ad541c3c2ead25a6
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-08-16 14:34:12 -0300

    novas aulas

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ objeto_cep = BuscaEndereco(cep)
 #r = requests.get("https://viacep.com.br/ws/01001000/json/")
 #print(r.text)
 
-a = objeto_cep.acessa_via_cep()
-print(type(a.text))
-print(a.json())
-print(a.json()['cep'])
-print(a.json()['bairro'])
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+
+print(bairro, cidade, uf)
