66ce6240ff58ae0b3f37a433d50dce21f14d9eef
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-08-16 14:26:53 -0300

    novas aulas

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import requests
 from acesso_cep import BuscaEndereco
 
-cep = 25870145
+cep = "01001000"
 objeto_cep = BuscaEndereco(cep)
-print(objeto_cep)
+
+#r = requests.get("https://viacep.com.br/ws/01001000/json/")
+#print(r.text)
+
+a = objeto_cep.acessa_via_cep()
+print(type(a.text))
+print(a.json())
+print(a.json()['cep'])
+print(a.json()['bairro'])
