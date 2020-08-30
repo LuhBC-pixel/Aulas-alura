d0357c7cc4c9412635f46ee537a49fee0123b18b
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-08-30 17:37:07 -0300

    novas aulas

--- aulaTeste/test_leilao.py
+++ aulaTeste/test_leilao.py
@@ -22,19 +22,15 @@ class TestLeilao(TestCase):
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
-    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
-        yuri = Usuario('Yuri')
-        
-        lance_do_yuri = Lance(yuri, 100.0)
-        
-        self.leilao.propoe(self.lance_do_gui)
-        self.leilao.propoe(lance_do_yuri)
+    def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
 
-        menor_valor_esperado = 100.0
-        maior_valor_esperado = 150.0
-
-        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
-        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+        with self.assertRaises(ValueError):
+            yuri = Usuario('Yuri')
+            
+            lance_do_yuri = Lance(yuri, 100.0)
+            
+            self.leilao.propoe(self.lance_do_gui)
+            self.leilao.propoe(lance_do_yuri)
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
         self.leilao.propoe(self.lance_do_gui)
@@ -66,7 +62,6 @@ class TestLeilao(TestCase):
 
         self.assertEqual(1, quantidade_de_lances_recebido)
 
-    # se o ultimo usuario for diferente, deve permitir propor um lance
     def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
         yuri = Usuario('Yuri')
         lance_do_yuri = Lance(yuri, 200.0)
@@ -78,13 +73,9 @@ class TestLeilao(TestCase):
 
         self.assertEqual(2, quantidade_de_lances_recebido)
 
-    # se o ultimo usuario for o mesmo, não deve permitir propor o lance
     def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
         lance_do_gui200 = Lance(self.gui, 200.0)
 
-        self.leilao.propoe(self.lance_do_gui)
-        self.leilao.propoe(lance_do_gui200)
-
-        quantidade_de_lances_recebido = len(self.leilao.lances)
-
-        self.assertEqual(1, quantidade_de_lances_recebido)
+        with self.assertRaises(ValueError):
+            self.leilao.propoe(self.lance_do_gui)
+            self.leilao.propoe(lance_do_gui200)
