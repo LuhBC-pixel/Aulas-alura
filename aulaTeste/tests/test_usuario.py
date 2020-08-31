0f029b9e128411c2605e0b97982729639ee7fa6f
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-08-30 21:17:41 -0300

    a

--- aulaTeste/tests/test_usuario.py
+++ aulaTeste/tests/test_usuario.py
@@ -1,4 +1,5 @@
 from aulaTeste.src.leilao.dominio import Usuario, Leilao
+from pytest import *
 
 def test_deve_substrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance():
     vini = Usuario('Vini', 100.0)
