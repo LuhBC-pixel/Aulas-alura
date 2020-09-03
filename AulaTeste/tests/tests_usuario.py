f89b24c48d0a34f7d5fd434602f244d65738cbb2
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-09-03 15:50:46 -0300

    novas aulas

---
+++ AulaTeste/tests/tests_usuario.py
@@ -0,0 +1,20 @@
+from AulaTeste.src.leilao.dominio import Usuario, Leilao
+
+
+def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance():
+    vini = Usuario('Vini', 100.0)
+
+    leilao = Leilao('Celular')
+
+    vini.propoe_lance(leilao, 50.)
+
+    assert vini.carteira == 50.0
+
+def test_deve_permitir_propor_lance_quando_o_valor_eh_menor_que_o_valor_da_carteira():
+    vini = Usuario('Vini', 100.0)
+
+    leilao = Leilao('Celular')
+
+    vini.propoe_lance(leilao, 1.0)
+
+    assert vini.carteira == 99.0
