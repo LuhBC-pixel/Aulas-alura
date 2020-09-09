b74825404f7420a33c55387d7f89c03df756b21d
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-09-09 12:34:41 -0300

    Novas aulas

--- pythonio-projeto-inicial/principal.py
+++ pythonio-projeto-inicial/principal.py
@@ -1,11 +1,8 @@
 try:
-    arquivo_contatos = open('dados/contato.csv', encoding='latin_1', mode='w+')
-
-    for linha in arquivo_contatos:
-        print(linha, end='')
+    with open('dados/contatos.csv', encoding='latin_1') as arquivo_contatos:
+        for linha in arquivo_contatos:
+            print(linha, end='')
 except FileNotFoundError:
     print('Arquivo não encontrado')
 except PermissionError:
     print('Sem permissão de escrita')
-finally:
-    arquivo_contatos.close()
