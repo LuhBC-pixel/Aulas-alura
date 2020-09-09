6c31771b8c16d2922a0a4735464cea0c84eb2f6c
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-09-09 19:38:14 -0300

    convertendo para pickle

--- pythonio-projeto-inicial/principal.py
+++ pythonio-projeto-inicial/principal.py
@@ -1,7 +1,10 @@
 import contatos_utils
 
 try:
-    contatos = contatos_utils.csv_para_contatos('dados/contatos.csv')
+    #contatos = contatos_utils.csv_para_contatos('dados/contatos.csv')
+    #contatos_utils.contatos_para_pickle(contatos, 'dados/contatos.pickle')
+
+    contatos = contatos_utils.pickle_para_contatos('dados/contatos.pickle')
 
     for contato in contatos:
         print(f'{contato.id} - {contato.nome} - {contato.email}')
