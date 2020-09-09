4491aaba774f518ee4ee8aa120b1d5fa11fc232a
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-09-09 14:08:16 -0300

    Create testa_io.py

---
+++ pythonio-projeto-inicial/testa_io.py
@@ -0,0 +1,11 @@
+arquivo = open('dados/contatos.csv', encoding='latin_1')
+
+print(type(arquivo.buffer))
+
+conteudo = arquivo.buffer.read()
+
+texto_em_bytes = bytes('Esse é um texto em bytes', 'latin_1')
+print(texto_em_bytes)
+print(type(texto_em_bytes))
+
+arquivo.close()
