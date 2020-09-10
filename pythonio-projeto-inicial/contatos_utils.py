89a3b704d9de5ce481349c8cbf54a315e3dc79ab
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-09-09 22:03:29 -0300

    Convertendo para json

--- pythonio-projeto-inicial/contatos_utils.py
+++ pythonio-projeto-inicial/contatos_utils.py
@@ -1,4 +1,4 @@
-import csv, pickle
+import csv, pickle, json
 from contato import Contato
 
 def csv_para_contatos(caminho, encoding='latin_1'):
@@ -22,5 +22,25 @@ def contatos_para_pickle(contatos, caminho):
 def pickle_para_contatos(caminho):
     with open(caminho, mode='rb') as arquivo:
         contatos = pickle.load(arquivo)
+
+    return contatos
+
+def contatos_para_json(contatos, caminho):
+    with open(caminho, mode='w') as arquivo:
+        json.dump(contatos, arquivo, default=_contato_para_json)
+
+def _contato_para_json(contato):
+    return contato.__dict__
+
+def json_para_contatos(caminho):
+    contatos = []
+
+    with open(caminho) as arquivo:
+        contatos_json = json.load(arquivo)
+
+        for contato in contatos_json:
+# ** significa desempacotar, exemplo, em vez de **, seria id=contato['id'], nome=contato['nome'] e assim por diante
+            c = Contato(**contato)
+            contatos.append(c)
 
     return contatos
