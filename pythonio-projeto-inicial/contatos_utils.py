1031d11eb61d43c12e6c3e8c28654da13376dfa9
LuhBC-pixel <bc.luisa@yahoo.com.br>
2020-09-09 22:32:19 -0300

    Cria classes para separar os tipos de arquivos

--- pythonio-projeto-inicial/contatos_utils.py
+++ pythonio-projeto-inicial/contatos_utils.py
@@ -1,43 +1,65 @@
 import csv, pickle, json
 from contato import Contato
+from abc import ABC, abstractmethod
 
-def csv_para_contatos(caminho, encoding='latin_1'):
-    contatos = []
+class ContatoDao(ABC):
 
-    with open(caminho, encoding=encoding) as arquivo:
-        leitor = csv.reader(arquivo)
+    @abstractmethod
+    def buscar_todos(self, caminho):
+        pass
 
-        for linha in leitor:
-            id, nome, email = linha
+    @abstractmethod
+    def salvar(self, contatos, caminho):
+        pass
 
-            contato = Contato(id, nome, email)
-            contatos.append(contato)
+class ContatoDaoCSV(ContatoDao):
 
-    return contatos
+    @abstractmethod
+    def buscar_todos(self, caminho, encoding='latin_1'):
+        contatos = []
 
-def contatos_para_pickle(contatos, caminho):
-    with open(caminho, mode='wb') as arquivo:
-        pickle.dump(contatos, arquivo)
+        with open(caminho, encoding=encoding) as arquivo:
+            leitor = csv.reader(arquivo)
 
-def pickle_para_contatos(caminho):
-    with open(caminho, mode='rb') as arquivo:
-        contatos = pickle.load(arquivo)
+            for linha in leitor:
+                id, nome, email = linha
 
-    return contatos
+                contato = Contato(id, nome, email)
+                contatos.append(contato)
 
-def contatos_para_json(contatos, caminho):
-    with open(caminho, mode='w') as arquivo:
-        json.dump(contatos, arquivo, default=lambda contato:contatos.__dict__)
+        return contatos
 
-def json_para_contatos(caminho):
-    contatos = []
+class ContatoDaoPickle(ContatoDao):
 
-    with open(caminho) as arquivo:
-        contatos_json = json.load(arquivo)
+    @abstractmethod
+    def buscar_todos(self, caminho):
+        with open(caminho, mode='rb') as arquivo:
+            contatos = pickle.load(arquivo)
 
-        for contato in contatos_json:
-# ** significa desempacotar, exemplo, em vez de **, seria id=contato['id'], nome=contato['nome'] e assim por diante
-            c = Contato(**contato)
-            contatos.append(c)
+        return contatos
 
-    return contatos
+    @abstractmethod
+    def salvar(self, contatos, caminho):
+        with open(caminho, mode='wb') as arquivo:
+            pickle.dump(contatos, arquivo)
+
+class ContatoDaoJSON(ContatoDao):
+
+    @abstractmethod
+    def buscar_todos(self, caminho):
+        contatos = []
+
+        with open(caminho) as arquivo:
+            contatos_json = json.load(arquivo)
+
+            for contato in contatos_json:
+            # ** significa desempacotar, exemplo, em vez de **, seria id=contato['id'], nome=contato['nome'] e assim por diante
+                c = Contato(**contato)
+                contatos.append(c)
+
+        return contatos
+
+    @abstractmethod
+    def salvar(self, contatos, caminho):
+        with open(caminho, mode='w') as arquivo:
+            json.dump(contatos, arquivo, default=lambda contato:contatos.__dict__)
